a7952501d3b3e83a3e3873ea13220d484dde98fd
Akar Khare <akarkhare02@gmail.com>
2023-09-11 21:31:54 +0530

    11/9 commit

--- client/src/Components/loading.js
+++ client/src/Components/loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FallingLines } from  'react-loader-spinner'
 const Loading = () => {
   return (
-    <div style={{width:"100%",border:"2px solid black",display :"flex",justifyContent:"center"}}><FallingLines
+    <div style={{width:"100%",paddingTop:"50px",display :"flex",justifyContent:"center"}}><FallingLines
     color="#4fa94d"
     width="100"
     visible={true}
