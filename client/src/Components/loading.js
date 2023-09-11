ead1acb61dbbdf67cc6e855eafbd172b2f0658bc
Akar Khare <akarkhare02@gmail.com>
2023-09-11 21:34:28 +0530

    11/9 commit

--- client/src/Components/loading.js
+++ client/src/Components/loading.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { FallingLines } from  'react-loader-spinner'
 const Loading = () => {
   return (
-    <div style={{width:"100%",paddingTop:"50px",display :"flex",justifyContent:"center"}}><FallingLines
-    color="#4fa94d"
+    <div style={{width:"100%",paddingTop:"50px",display :"flex",justifyContent:"center"}}><Rings
+    height="100"
     width="100"
+    color="#4fa94d"
+    radius="6"
+    wrapperStyle={{}}
+    wrapperClass=""
     visible={true}
-    ariaLabel='falling-lines-loading'
+    ariaLabel="rings-loading"
   /></div>
   )
 }
